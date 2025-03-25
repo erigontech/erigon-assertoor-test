76b679eee1e7c83ca20cab1b4eed4e7ee9a96084
tosettil-polimi <info@tosettil.me>
2025-03-25 10:31:15 +0100

    feat(script): update kurtosis command to specific version 4.6.0

--- run_kurtosis.sh
+++ run_kurtosis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kurtosis run github.com/ethpandaops/ethereum-package --args-file ./example.yml --enclave my-testnet
+kurtosis run github.com/ethpandaops/ethereum-package@4.6.0 --args-file ./example.yml --enclave my-testnet
 exit 0
